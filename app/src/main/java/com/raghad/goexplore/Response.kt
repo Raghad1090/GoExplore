2b804ea37278d7c844454e6e8cd76349d57e9476
raghad <RAGHAD.MOHAMMED.ruh.k@tuwaiq.edu.sa>
2021-12-20 15:24:06 +0300

    gitflow-feature-stash: login

---
+++ app/src/main/java/com/raghad/goexplore/Response.kt
@@ -0,0 +1,66 @@
+package com.raghad.goexplore
+
+import com.squareup.moshi.Json
+
+data class Response(
+
+	@Json(name="stat")
+	val stat: String? = null,
+
+	@Json(name="photos")
+	val photos: Photos? = null
+)
+
+data class PhotoItem(
+
+	@Json(name="owner")
+	val owner: String? = null,
+
+	@Json(name="server")
+	val server: String? = null,
+
+	@Json(name="is_primary")
+	val isPrimary: Int? = null,
+
+	@Json(name="has_comment")
+	val hasComment: Int? = null,
+
+	@Json(name="ispublic")
+	val ispublic: Int? = null,
+
+	@Json(name="isfriend")
+	val isfriend: Int? = null,
+
+	@Json(name="farm")
+	val farm: Int? = null,
+
+	@Json(name="id")
+	val id: String? = null,
+
+	@Json(name="secret")
+	val secret: String? = null,
+
+	@Json(name="title")
+	val title: String? = null,
+
+	@Json(name="isfamily")
+	val isfamily: Int? = null
+)
+
+data class Photos(
+
+	@Json(name="perpage")
+	val perpage: Int? = null,
+
+	@Json(name="total")
+	val total: Int? = null,
+
+	@Json(name="pages")
+	val pages: Int? = null,
+
+	@Json(name="photo")
+	val photo: List<PhotoItem?>? = null,
+
+	@Json(name="page")
+	val page: Int? = null
+)
